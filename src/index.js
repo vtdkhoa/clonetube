c3d4a59362d7e000e5d2e4494d3d22ebb4b9544b
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-07-22 10:46:16 +0700

    [Initial]: Initialize store configuration

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from './store/configureStore'
+import 'semantic-ui-css/semantic.min.css'
 
-// semantic-ui-css
-import 'semantic-ui-css/semantic.min.css';
+const store = configureStore()
 
 const app = (
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
