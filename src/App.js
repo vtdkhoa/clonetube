4eb5ed380a2b46c7a130777e3f844e0ba7064a67
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-07-22 15:04:33 +0700

    [Initial]: Initialize youtube api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import AppLayout from './components/AppLayout/AppLayout'
 import Home from './containers/Home/Home'
 import Watch from './containers/Watch/Watch'
 import { Switch, Route } from 'react-router-dom'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { youtubeLibraryLoaded } from './store/actions/api'
+
+const API_KEY = 'AIzaSyBsqBssd8yrFDMcdKOy7vxhjglCIqmMZJA'
 
 class App extends Component {
+  componentDidMount() {
+    this.loadYoutubeApi()
+  }
+
+  loadYoutubeApi() {
+    const script = document.createElement('script')
+    script.src = 'https://apis.google.com/js/client.js'
+
+    script.onload = () => {
+      window.gapi.load('client', () => {
+        window.gapi.client.setApiKey(API_KEY)
+        window.gapi.client.load('youtube', 'v3', () => {
+          this.props.youtubeLibraryLoaded()
+        })
+      })
+    }
+    document.body.appendChild(script)
+  }
+
   render() {
     return(
       <AppLayout>
@@ -17,4 +41,10 @@ class App extends Component {
   }
 }
 
-export default App
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    youtubeLibraryLoaded
+  }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(App)
---
+++ src/store/reducers/api.js
@@ -0,0 +1,18 @@
+import * as types from '../constants'
+
+const initialState = {
+  libraryLoaded: false
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case types.YOUTUBE_LIBRARY_LOADED:
+      return {
+        libraryLoaded: true
+      }
+    default:
+      return state
+  }
+}
+
+export const getYoutubeLibraryLoaded = state => state.api.libraryLoaded
