f213c39e423315f7ecdc11e5915ed1bdbfd454c2
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-07-28 17:20:00 +0700

    [Update]: Components (App and HomeContent) unit tests

--- src/containers/Home/HomeContent/__tests__/HomeContent.unit.test.js
+++ src/containers/Home/HomeContent/__tests__/HomeContent.unit.test.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import HomeContent from '../HomeContent'
+import { Provider } from 'react-redux'
 import { shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
+
+const mockStore = configureStore()
+const store = mockStore({
+  mostPopularVideos: [],
+  videosByCategory: []
+})
 
 describe('HomeContent', () => {
   test('renders', () => {
-    const wrapper = shallow(<HomeContent/>)
+    const wrapper = shallow(
+      <Provider store={store}>
+        <HomeContent
+          bottomReachedCallback={jest.fn()}
+          showLoader={true}
+        />
+      </Provider>
+    )
     expect(wrapper).toMatchSnapshot()
   })
 })
