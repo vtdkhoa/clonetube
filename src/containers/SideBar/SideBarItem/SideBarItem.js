6c05c537fdf37386b636960620e50537a102dcca
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-08-10 16:06:37 +0700

    Handling dynamic Sidebar [Treding videos]

--- src/containers/SideBar/SideBarItem/SideBarItem.js
+++ src/containers/SideBar/SideBarItem/SideBarItem.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Icon, Menu } from 'semantic-ui-react'
+import { Link, withRouter } from 'react-router-dom'
 import './SideBarItem.scss'
 
-const SideBarItem = props => {
-  const highlight = props.highlight ? 'highlight-item' : null
+class SideBarItem extends Component {
+  shouldBeHighlighed() {
+    const { pathname } = this.props.location
 
-  return (
-    <Menu.Item className={['sidebar-item', highlight].join(' ')}>
-      <div className="sidebar-item-alignment-container">
-        <span>
-          <Icon size="large" name={props.icon}/>
-        </span>
-        <span>{props.label}</span>
-      </div>
-    </Menu.Item>
-  )
+    if (this.props.path === '/') {
+      return pathname === this.props.path
+    }
+    return pathname.includes(this.props.path)
+  }
+
+  render() {
+    // React will ignore custom boolean attributes. therefore I pass a string
+    // by using this attribute in our SCSS for styling
+    const highlight = this.shouldBeHighlighed() ? 'highlight-item' : null
+
+    return (
+      <Link to={{ pathname: this.props.path }}>
+        <Menu.Item className={['sidebar-item', highlight].join(' ')}>
+          <div className="sidebar-item-alignment-container">
+            <span>
+              <Icon size="large" name={this.props.icon}/>
+            </span>
+            <span>{this.props.label}</span>
+          </div>
+        </Menu.Item>
+      </Link>
+    )
+  }
 }
 
-export default SideBarItem
+export default withRouter(SideBarItem)
